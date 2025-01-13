87ebbacbcf350dee075c0f3ea092e76d649011bc
Tomas <Tomasgruca1@gmail.com>
2025-01-13 14:21:22 +0000

    added registration on firebase

--- touch_grass/lib/screens/signup.dart
+++ touch_grass/lib/screens/signup.dart
@@ -1,42 +1,64 @@
 import 'package:touch_grass/services/auth.dart';
 import 'package:flutter/material.dart';
+import 'package:touch_grass/authenicate/register.dart';
 
-class Signup extends StatefulWidget{
+class Signup extends StatefulWidget {
   @override
   _SignupState createState() => _SignupState();
 }
 
-class _SignupState extends State<Signup>{
+class _SignupState extends State<Signup> {
+  final AuthService _auth = AuthService();
 
-final AuthService _auth = AuthService();
+  String email = '';
+  String password = '';
 
   @override
-  Widget build(BuildContext context){
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('Sign Up'),
-        backgroundColor: Color(0xFFbfd37a),
+        title: const Text('Sign Up Page which is the signup.dart file'),
+        backgroundColor: const Color(0xFFbfd37a),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            ElevatedButton(
-              child: Text('Sign in Anon'),
-              onPressed: () async {
-                dynamic result = await _auth.signInAnon();
-                if (result == null){
-                  print('error signing in');
-                } else {
-                  print('signed in');
-                  print(result);
-                }
-              }
-            ),
-          ],
+        child: Form(
+          child: Column(
+            children: <Widget>[
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Email',
+                ),
+                onChanged: (val) {
+                  setState(() => email = val);
+                },
+              ),
+              const SizedBox(height: 20.0),
+              TextFormField(
+                decoration: const InputDecoration(
+                  hintText: 'Password',
+                ),
+                obscureText: true,
+                onChanged: (val) {
+                  setState(() => password = val);
+                },
+              ),
+              SizedBox(height: 20.0),
+             const SizedBox(height: 20.0),
+              ElevatedButton(
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => Register()),
+                  );
+                },
+                child: const Text('Don\'t have an account? Register here'),
+
+              ),
+            ],
+          ),
         ),
       ),
     );
-
   }
 }
